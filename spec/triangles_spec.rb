0fde781e5cc3a702cc8b0675bcdf76e8b33c79e1
Brian Harvey <brian.harv3y@gmail.com>
2020-09-28 14:51:25 -0700

    write test 3 spec
    Co-authored-by: Drew Parker <drewjparker91@gmail.com>

--- spec/triangles_spec.rb
+++ spec/triangles_spec.rb
@@ -15,6 +15,13 @@ describe ('Triangle#equilateral') do
   end
 end
 
+describe ('Triangle#equilateral') do
+  it('checks to see if three sides create isosceles triangle') do
+    tc = Triangle.new(4, 4, 4)
+      expect(tc.isosc()).to(eq(true))
+  end
+end
+
 # describe '#Album' do
 #   before(:each) do
 #     Triangle.clear()
